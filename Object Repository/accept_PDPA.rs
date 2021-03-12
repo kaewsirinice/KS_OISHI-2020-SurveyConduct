1b978723ab802e0a199150736c74219e2c1d97d4
kaewsirinice <58505285+kaewsirinice@users.noreply.github.com>
2021-03-12 11:38:53 +0700

    Version 1.0.0

---
+++ Object Repository/accept_PDPA.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>accept_PDPA</name>
+   <tag></tag>
+   <elementGuidId>0fd365fc-9c37-4167-8f5d-596a6d646c60</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='question_element']//div[2]//label[1]//label[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='question_element']//div[2]//label[1]//label[1]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/radio_choice.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>radio_choice</name>
+   <tag></tag>
+   <elementGuidId>45941732-39ee-4f0d-ab90-b9bcada74dd6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='choices']//div[1]//div[${buff}]//label[1]//label[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='choices']//div[1]//div[${buff}]//label[1]//label[1]</value>
+   </webElementProperties>
+</WebElementEntity>
